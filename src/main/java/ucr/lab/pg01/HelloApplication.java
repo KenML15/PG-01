87d20ab49214ba80abbb42a7fd8e6f11effddaac
pablo <pablosolanov07@gmail.com>
2026-03-20 17:06:16 -0600

    interfaz fibonacci

--- src/main/java/ucr/lab/pg01/HelloApplication.java
+++ src/main/java/ucr/lab/pg01/HelloApplication.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,13 +13,16 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    public static void main(String[] args) {
+
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         //start1(stage);
         start2(stage);
     }
-
-
 
     public void start2(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
@@ -35,30 +37,25 @@ public class HelloApplication extends Application {
 
     public void start1(Stage stage) {
 
-        TextField name= new TextField("1000");
-        Label result= new Label("");
-        Button button=new Button("medir O(n) vs O(n^2)");
+        TextField name = new TextField("1000");
+        Label result = new Label("");
+        Button button = new Button("medir O(n) vs O(n^2)");
 
         button.setOnAction(actionEvent -> {
-            int n= Integer.parseInt(name.getText());
+            int n = Integer.parseInt(name.getText());
             /**
              * ahora vamos a determinar el tiempo de ejecución de un algoritmo
              */
 
-            long t1= System.nanoTime();
-            long sum=0;
-            for(int i=1; i<=n;i++){
-                sum+=i; //O(n)
+            long t1 = System.nanoTime();
+            long sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i; //O(n)
             }
-            long t2= System.nanoTime();
-            result.setText("O(n): "+util.Utility.format(t2-t1)+" ns");
+            long t2 = System.nanoTime();
+            result.setText("O(n): " + util.Utility.format(t2 - t1) + " ns");
 
         });
 
-    }
-
-    public static void main(String[] args) {
-
-        launch();
     }
 }
